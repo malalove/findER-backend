9c927872cd398f3c55e3a36653825992de3334ec
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2023-11-02 13:43:27 +0900

    refactor: BedRepository 리팩터링

--- src/main/java/com/finder/repository/BedRepository.java
+++ src/main/java/com/finder/repository/BedRepository.java
@@ -9,29 +9,17 @@ import java.util.List;
 
 public interface BedRepository extends JpaRepository<Bed, BedId> {
 
-    @Query("SELECT b FROM Bed b WHERE b.name = :name " +
-            "AND EXTRACT(YEAR FROM b.localDateTime) = EXTRACT(YEAR FROM :time) " +
-            "AND EXTRACT(MONTH FROM b.localDateTime) = EXTRACT(MONTH FROM :time) " +
-            "AND EXTRACT(DAY FROM b.localDateTime) = EXTRACT(DAY FROM :time) " +
-            "AND EXTRACT(HOUR FROM b.localDateTime) = EXTRACT(HOUR FROM :time) " +
-            "AND EXTRACT(MINUTE FROM b.localDateTime) = EXTRACT(MINUTE FROM :time)")
+    @Query(value = "select b from Bed b where b.name = :name " +
+            "and EXTRACT(YEAR FROM b.localDateTime) = EXTRACT(YEAR FROM :time) " +
+            "and EXTRACT(MONTH FROM b.localDateTime) = EXTRACT(MONTH FROM :time) " +
+            "and EXTRACT(DAY FROM b.localDateTime) = EXTRACT(DAY FROM :time) " +
+            "and EXTRACT(HOUR FROM b.localDateTime) = EXTRACT(HOUR FROM :time) " +
+            "and EXTRACT(MINUTE FROM b.localDateTime) = EXTRACT(MINUTE FROM :time)")
     Bed findByNameAndTime(String name, LocalDateTime time);
 
-    @Query("select b " +
+    @Query(value = "select b " +
             "from Bed b " +
             "where b.name = :name and " +
             "b.localDateTime between :beforeTime and :currentTime")
     List<Bed> findByRecent(String name, LocalDateTime beforeTime, LocalDateTime currentTime);
-
-//    @Query("select b " +
-//            "from Bed b " +
-//            "where b.name = :name and " +
-//            "b.time between :beforeTime and :currentTime " +
-//            "and FUNCTION('MINUTE', b.time) IN (0, 15, 40, 45)")
-    @Query(value = "SELECT * " +
-        "FROM bed " +
-        "WHERE name = :name " +
-        "AND localDateTime BETWEEN :beforeTime AND :currentTime " +
-        "AND MOD(TIMESTAMPDIFF(MINUTE, :beforeTime, time), 15) = 0", nativeQuery = true)
-    List<Bed> findByNewRecent(String name, LocalDateTime beforeTime, LocalDateTime currentTime);
 }
